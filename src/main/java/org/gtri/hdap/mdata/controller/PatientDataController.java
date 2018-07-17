bbed580b6571c8a0ede481c296dfe24b20e34c23
es130 <eric.soto@gtri.gatech.edu>
2018-07-17 09:56:00 -0400

    Updated PatientDataController to have method to retrieve data

--- src/main/java/org/gtri/hdap/mdata/controller/PatientDataController.java
+++ src/main/java/org/gtri/hdap/mdata/controller/PatientDataController.java
@@ -7,25 +7,21 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
+import org.gtri.hdap.mdata.jpa.entity.ApplicationUser;
+import org.gtri.hdap.mdata.jpa.entity.ShimmerData;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.net.ssl.SSLContext;
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -37,7 +33,9 @@ public class PatientDataController {
     /*========================================================================*/
     /* Constants */
     /*========================================================================*/
-    public static String SHIMMER_SERVER_URL_ENV = "SHIMMER_SERVER_URL";
+    public static String SHIMMER_SERVER_URL_BASE_ENV = "SHIMMER_SERVER_URL";
+    public static String SHIMMER_AUTH_URL = "/authorize/{shimKey}?username={username}";
+    public static String SHIMMER_DATA_RANGE_URL = "/data/{shim-key}/physical_activity?username={username}&normalize={normalize}&dateStart={start-date}&dateEnd={end-date}";
 
     /*========================================================================*/
     /* Variables */
@@ -93,9 +91,9 @@ public class PatientDataController {
         // for example https://gt-apps.hdap.gatech.edu:8083/authorize/fitbit?username={userId}
         // The username query parameter can be set to any unique identifier you'd like to use to identify the user.
 
-        String shimmerAuthUrl = System.getenv(SHIMMER_SERVER_URL_ENV);
+        String shimmerAuthUrl = System.getenv(SHIMMER_SERVER_URL_BASE_ENV) + SHIMMER_AUTH_URL;
         shimmerAuthUrl = shimmerAuthUrl.replace("{shimKey}", "fitbit");
-        shimmerAuthUrl = shimmerAuthUrl.replace("{userId}", "3f6625db-8cc7-4d25-9bf4-9febdc7028cd");
+        shimmerAuthUrl = shimmerAuthUrl.replace("{username}", "3f6625db-8cc7-4d25-9bf4-9febdc7028cd");
         HttpGet httpGet = new HttpGet(shimmerAuthUrl);
         String fitbitAuthUrl = processShimmerAuthRequest(httpGet);
         if( fitbitAuthUrl == null ){
@@ -107,6 +105,27 @@ public class PatientDataController {
 //        return "This page handles shimmer user auth";
         String redirectUrl = "redirect:" + fitbitAuthUrl;
         return new ModelAndView(redirectUrl, model);
+    }
+
+    @GetMapping("/activity")
+    public ResponseEntity<?> retrievePatientData(){
+        String shimmerAuthUrl = System.getenv(SHIMMER_SERVER_URL_BASE_ENV) + SHIMMER_AUTH_URL;
+        shimmerAuthUrl = shimmerAuthUrl.replace("{shimKey}", "fitbit");
+        shimmerAuthUrl = shimmerAuthUrl.replace("{username}", "3f6625db-8cc7-4d25-9bf4-9febdc7028cd");
+        shimmerAuthUrl = shimmerAuthUrl.replace("{normalize}", "true");
+        shimmerAuthUrl = shimmerAuthUrl.replace("{start-date}", "2018-06-01");
+        shimmerAuthUrl = shimmerAuthUrl.replace("{end-date}", "2018-07-17");
+        HttpGet httpGet = new HttpGet(shimmerAuthUrl);
+        String jsonResponse = processShimmerDataRequest(httpGet);
+        if(jsonResponse == null){
+            //return a 500 error
+            logger.debug("Did not find data");
+        }
+
+        //TODO replace this code with queries to JPA
+        ApplicationUser appUser = new ApplicationUser("ehr1234", "3f6625db-8cc7-4d25-9bf4-9febdc7028cd");
+        ShimmerData shimmerData = new ShimmerData(appUser, jsonResponse);
+        return ResponseEntity.ok(shimmerData);
     }
 
     @RequestMapping("/authorize/fitbit/callback")
@@ -168,5 +187,37 @@ public class PatientDataController {
             shimmerAuthResponse.close();
         }
         return authorizationUrl;
+    }
+
+    private String processShimmerDataRequest(HttpUriRequest request){
+        String jsonResp = null;
+        try {
+            CloseableHttpResponse shimmerAuthResponse = getHttpClient().execute(request, getHttpClientContext());
+            jsonResp = checkShimmerAuthResponse(shimmerAuthResponse);
+        }
+        catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+        return jsonResp;
+    }
+
+    private String checkShimmerDataResponse(CloseableHttpResponse shimmerAuthResponse) throws IOException {
+        String jsonResp = null;
+        try {
+            int statusCode = shimmerAuthResponse.getStatusLine().getStatusCode();
+            logger.debug("Response Code " + statusCode);
+            if(statusCode == 200) {
+                //All we need is the cookie, which is managed as part of the HTTPContext
+                //for now ignore the content of the response. At a later date process
+                //the JSON. It contains permission and user metadata.
+                HttpEntity responseEntity = shimmerAuthResponse.getEntity();
+                //get the json from the response and get the auth URL to redirect the user
+                jsonResp = EntityUtils.toString(responseEntity);
+                logger.debug("Data Response: " + jsonResp);
+            }
+        } finally {
+            shimmerAuthResponse.close();
+        }
+        return jsonResp;
     }
 }
