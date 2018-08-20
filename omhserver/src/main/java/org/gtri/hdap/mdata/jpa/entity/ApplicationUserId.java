72100b85d6c143a990894d6ca37eb4ec7a47a1bf
es130 <eric.soto@gtri.gatech.edu>
2018-08-20 11:17:15 -0400

    Added ApplicationUserId entity to be used as an EmbeddedId in the ApplicationUser Entity object.

---
+++ omhserver/src/main/java/org/gtri/hdap/mdata/jpa/entity/ApplicationUserId.java
@@ -0,0 +1,51 @@
+package org.gtri.hdap.mdata.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+/**
+ * Created by es130 on 8/20/2018.
+ */
+@Embeddable
+public class ApplicationUserId implements Serializable{
+
+    /*========================================================================*/
+    /* Variables */
+    /*========================================================================*/
+    @Column(name="ehr_id")
+    private String ehrId;
+    @Column(name="shim_key")
+    private String shimKey;
+
+    /*========================================================================*/
+    /* Constructors */
+    /*========================================================================*/
+    public ApplicationUserId(String ehrId, String shimKey) {
+        this.ehrId = ehrId;
+        this.shimKey = shimKey;
+    }
+
+    /*========================================================================*/
+    /* Getters */
+    /*========================================================================*/
+    public String getEhrId() {
+        return ehrId;
+    }
+
+    public String getShimKey() {
+        return shimKey;
+    }
+
+    /*========================================================================*/
+    /* Setters */
+    /*========================================================================*/
+    public void setEhrId(String ehrId) {
+        this.ehrId = ehrId;
+    }
+
+    public void setShimKey(String shimKey) {
+        this.shimKey = shimKey;
+    }
+}
