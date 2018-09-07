b063b0f645e989d9647af9d08e77cca12ca35340
Eric Soto <eric.gabriel.soto@gmail.com>
2018-09-07 13:29:32 -0400

    Updated login to launch OAuth in new window

--- omhclient/app/components/login/login.component.js
+++ omhclient/app/components/login/login.component.js
@@ -5,12 +5,13 @@ module('login').
 component('login', {
 
     templateUrl: 'components/login/login.template.html',
-    controller: ['$scope', '$http', '$routeParams', 'OmhOnFhirApi', '__env', function LoginController($scope, $http, $routeParams, OmhOnFhirApi, env){
+    controller: ['$scope', '$http', '$window', '$location', '$routeParams', 'OmhOnFhirApi', '__env', function LoginController($scope, $http, $window, $location, $routeParams, OmhOnFhirApi, env){
         var self = this;
         self.env = env;
         self.omhOnFhirApi = OmhOnFhirApi;
 
         self.pageMsg = 'TODO make login page';
+        self.googleOauthUrl;
 
         console.log("Params passed to login");
         console.log($routeParams);
@@ -34,6 +35,20 @@ component('login', {
             console.log(error);
         });
 
+        //When the window gets focus check to see if authentication through OAuth was successful.
+        //If so, forward to activity page, otherwise remain on login page.
+        $window.onfocus = function (){
+            console.log("Login window has focus");
+            if( self.omhOnFhirApi.loginSuccessful ){
+                //forward to the activity page
+                console.log("Authentication successful redirecting to " + self.env.baseUrl + "activity");
+                $location.path(self.env.baseUrl + "activity");
+            }
+            else{
+                console.log("Login not successful");
+            }
+        };
+
         //===================================================================================
         // Functions
         //===================================================================================
@@ -44,6 +59,12 @@ component('login', {
 
         self.loginWithGoogleFit = function loginWithGoogleFit(){
             console.log("Logging in with Google fit");
+            //self.googleOauthUrl =
+            //    self.env.omhOnFhirAPIBase +
+            //    self.env.omhOnFhirAPIShimmerAuth +
+            //    "?ehrId=" + self.omhOnFhirApi.getPatientId() +
+            //    "&shimkey=" + self.env.googleFitShim;
+            //console.log("Opening Google OAuth URL: " + self.googleOauthUrl);
             self.omhOnFhirApi.login(self.env.googleFitShim);
         };
 
