a5a99276a362ef4daa11e5f42483a25f900ff1fd
Eric Soto <eric.gabriel.soto@gmail.com>
2018-09-12 15:38:25 -0400

    Added equals and hashcode to ApplicationUserId composite key.

--- omhserver/src/main/java/org/gtri/hdap/mdata/jpa/entity/ApplicationUserId.java
+++ omhserver/src/main/java/org/gtri/hdap/mdata/jpa/entity/ApplicationUserId.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Created by es130 on 8/20/2018.
@@ -48,5 +49,19 @@ public class ApplicationUserId implements Serializable{
 
     public void setShimKey(String shimKey) {
         this.shimKey = shimKey;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ApplicationUserId that = (ApplicationUserId) o;
+        return Objects.equals(getEhrId(), that.getEhrId()) &&
+                Objects.equals(getShimKey(), that.getShimKey());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEhrId(), getShimKey());
     }
 }
