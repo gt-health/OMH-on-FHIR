66e6ae467694838455bff061c503d22edd3b285a
Eduardo Zuloaga <eduardo.zuloaga.ctr@gtri.gatech.edu>
2019-05-09 16:30:31 -0400

    Fixed environment variables for rancher environment

--- omhclient/app/js/env.js
+++ omhclient/app/js/env.js
@@ -6,7 +6,7 @@
     window.__env.googleFitShim = 'googlefit';
     window.__env.omhOnFhirClientId = '93651a15-4664-486e-8661-eca7ebc21bda';
     window.__env.omhOnFhirScope = 'patient/*.read launch'; //to force provider login use the following scopes 'openid profile'
-    window.__env.omhOnFhirRedirectUri = 'http://localhost:8989/omhonfhir/login';
-    window.__env.omhOnFhirAPIBase = 'http://localhost:8888/mdata';
+    window.__env.omhOnFhirRedirectUri = 'https://apps.hdap.gatech.edu/omhonfhir/login';
+    window.__env.omhOnFhirAPIBase = 'https://apps.hdap.gatech.edu/mdata';
     window.__env.omhOnFhirAPIShimmerAuth = '/shimmerAuthentication';
 }(this));
