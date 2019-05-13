fc52425b7cbd0faa50e8531004a75c3366d51ce0
es130 <eric.soto@gtri.gatech.edu>
2019-05-13 11:04:16 -0400

    Refactored InitDatabase.java

--- omhserver/src/main/java/org/gtri/hdap/mdata/InitDatabase.java
+++ omhserver/src/main/java/org/gtri/hdap/mdata/InitDatabase.java
@@ -1,7 +1,6 @@
 package org.gtri.hdap.mdata;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.gtri.hdap.mdata.common.jpa.entity.FhirTemplate;
 import org.gtri.hdap.mdata.common.jpa.entity.ResourceConfig;
@@ -17,12 +16,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import sun.nio.ch.IOUtil;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -37,16 +35,23 @@ public class InitDatabase {
     public static String CATALOG_JSON_MAPPING_ARRAY_NAME = "mappings";
     public static String CATALOG_JSON_MAPPING_TEMPLATE_ELEMENT_NAME = "template";
     public static String CATALOG_JSON_MAPPING_CONFIG_ELEMENT_NAME = "config";
+    public static String JSON_FILE_PATTERN = "\\w+/(\\w+_[\\w-]+).json";
 
     /*========================================================================*/
 	/* VARIABLES */
     /*========================================================================*/
     private static final Logger logger = LoggerFactory.getLogger(InitDatabase.class);
+    private Pattern jsonFilePattern = Pattern.compile(JSON_FILE_PATTERN);
 
+    /**
+     * Runs at application start up to load the config files into the database
+     * @param rcRepo the ResourceConfigRepository to use
+     * @param ftRepo the FhirTemplateRepository to use
+     * @return the function to call at start up
+     */
     @Bean
     CommandLineRunner loadData(ResourceConfigRepository rcRepo, FhirTemplateRepository ftRepo) {
         return args -> {
-
             logger.debug("Loading mapping config files");
             //load json templates and configs
             InputStream catalogInputStream = getClassPathResourceAsInputStream("catalog.json");;
@@ -55,6 +60,11 @@ public class InitDatabase {
         };
     }
 
+    /**
+     * Returns the InputStream for the resource located at resourcePath
+     * @param resourcePath the path to the classpath resource to locate
+     * @return an InputStream for the resource if it is found, or null otherwise
+     */
     private InputStream getClassPathResourceAsInputStream(String resourcePath){
         Resource configCatalogResource = new ClassPathResource(resourcePath);
         InputStream catalogInputStream = null;
@@ -67,6 +77,13 @@ public class InitDatabase {
         return catalogInputStream;
     }
 
+    /**
+     * Parses the catalog.json file that describes how config and template resources files are used.
+     * @param catalogInputStream the InputStream to the catalog
+     * @param rcRepo the ResourceConfigRepository to use
+     * @param ftRepo the FhirTemplateRepository to use
+     * @throws IOException thrown if the resource or template file on the classpath cannot be retrieved as an InputStream
+     */
     private void parseCatalogFile(InputStream catalogInputStream, ResourceConfigRepository rcRepo, FhirTemplateRepository ftRepo) throws IOException{
         logger.debug("Parsing Catalog File");
         if( catalogInputStream != null ) {
@@ -87,37 +104,72 @@ public class InitDatabase {
         logger.debug("Finished Parsing Catalog File");
     }
 
+    /**
+     * Parses a mapping entry in the catalog.json file.
+     * @param currMappingObject the mapping object to parse
+     * @param rcRepo the ResourceConfigRepository to use
+     * @param ftRepo the FhirTemplateRepository to use
+     * @throws IOException thrown if the resource or template file on the classpath cannot be retrieved as an InputStream
+     */
     private void parseMappingConfig(JSONObject currMappingObject, ResourceConfigRepository rcRepo, FhirTemplateRepository ftRepo) throws IOException{
         logger.debug("Parsing Mapping template");
         String currTemplate = currMappingObject.getString(CATALOG_JSON_MAPPING_TEMPLATE_ELEMENT_NAME);
         String currConfig = currMappingObject.getString(CATALOG_JSON_MAPPING_CONFIG_ELEMENT_NAME);
-        logger.debug("Curr Config " + currConfig);
+        saveResourceConfig(currConfig, rcRepo);
+        saveTemplateConfig(currTemplate, ftRepo);
+        logger.debug("Finished Parsing Mapping template");
+    }
 
-        //templates will be of the format resourceConfigs/dstu3_step-count.json
-        //parse teh config to get the
-        Pattern p = Pattern.compile("\\w+/(\\w+_[\\w-]+).json");
-        Matcher m = p.matcher(currConfig);
-        m.find();
-        String configName = m.group(1);
-        logger.debug("Name of config: " + configName);
+    /**
+     * Saves the resource config to the database
+     * @param configResourcePath the path to the resource config on the classpath
+     * @param rcRepo the ResourceConfigRepository to use
+     * @throws IOException thrown if the resource or template file on the classpath cannot be retrieved as an InputStream
+     */
+    private void saveResourceConfig(String configResourcePath, JpaRepository rcRepo) throws IOException{
+        logger.debug("Saving Resource Config");
+        String configName = getConfigResourceFileName(configResourcePath);
         ObjectMapper mapper = new ObjectMapper();
         ResourceConfig resourceConfig = new ResourceConfig();
         resourceConfig.setResourceId(configName);
-        InputStream in = getClassPathResourceAsInputStream(currConfig);
+        InputStream in = getClassPathResourceAsInputStream(configResourcePath);
         resourceConfig.setConfig(mapper.readTree(IOUtils.toString(in, UTF_8)));
         rcRepo.save(resourceConfig);
+        logger.debug("Finished Saving Resource Config");
+    }
 
-        logger.debug("Curr Template " + currTemplate);
-        m = p.matcher(currTemplate);
-        m.find();
-        configName = m.group(1);
-        logger.debug("Name of template: " + configName);
+    /**
+     * Saves the template config to the database
+     * @param templateResourcePath the path to the template config on the classpath
+     * @param ftRepo the FhirTemplateRepository to use
+     * @throws IOException thrown if the resource or template file on the classpath cannot be retrieved as an InputStream
+     */
+    private void saveTemplateConfig(String templateResourcePath, JpaRepository ftRepo) throws IOException{
+        logger.debug("Saving Template Config");
+        String configName = getConfigResourceFileName(templateResourcePath);
+        ObjectMapper mapper = new ObjectMapper();
         FhirTemplate fhirTemplate = new FhirTemplate();
         fhirTemplate.setTemplateId(configName);
-        in = getClassPathResourceAsInputStream(currTemplate);
+        InputStream in = getClassPathResourceAsInputStream(templateResourcePath);
         fhirTemplate.setTemplate(mapper.readTree(IOUtils.toString(in, UTF_8)));
         ftRepo.save(fhirTemplate);
-        logger.debug("Finished Parsing Mapping template");
+        logger.debug("Finished Saving Template Config");
+    }
+
+    /**
+     * Retrives the config filename from the full path
+     * @param configResourcePath the path to search
+     * @return the name of the config file.
+     */
+    private String getConfigResourceFileName(String configResourcePath){
+        //templates will be of the format resourceConfigs/dstu3_step-count.json
+        //parse the config to get the
+        logger.debug("Getting config file name from path");
+        Matcher m = jsonFilePattern.matcher(configResourcePath);
+        m.find();
+        String configName = m.group(1);
+        logger.debug("Config file name: " + configName);
+        return configName;
     }
 
 }
